ca8cb9faf4c534a334b17266a1e5aaa1a73edf50
Sebastian Bitzer <bitzer@cbs.mpg.de>
2011-11-03 10:49:03 +0100

    added "thinking about" to project name detection

--- activitylogAnalysis.m
+++ activitylogAnalysis.m
@@ -46,7 +46,19 @@ for d = 1:ndays
     for r = 1:length(rind)
         actdaily(d).acts(rind) = actdaily(d).acts(rind-2);
     end
+    
+    % replace undescriptive "paper" with "rrnn paper" when occuring in
+    % certain time interval
+    if actdaily(d).day < datenum(2011,08,01)
+        % also replace "project paper", but not "groupmeeting paper"
+        actdaily(d).acts = regexprep(actdaily(d).acts,...
+            '(project paper)|((?<!groupmeeting )paper)','rrnn paper');
+    end
 end
+
+actdays = cell(1,ndays);
+[actdays{:}] = actdaily.day;
+actdays = cell2mat(actdays);
 
 actall.acts = cell(nact,1);
 actall.durs = nan(nact,1);
@@ -197,10 +209,149 @@ vis3 = initvis([],[680 764 1050 340]);
 vis3.bar = bar(individdurs*24);
 xlabel('person')
 ylabel('meeting time in hours')
-set(gca,'XTickLabel',individname)
+set(gca,'XTick',1:length(individname),'XTickLabel',individname)
+xticklabel_rotate
 
 
 %% time spent working on "paper" (this is the rRNN paper)
 % paper time vs. other time
 
 % evolution of paper time per week
+
+
+%% time spent on different projects
+% what is a project name?
+% project names mentioned in: deriving ... for , reading, literature
+% search, making ... for , managing, preparing ... for , programming,
+% coding, thinking about, writing ... for , 
+
+% extract project names after occurrence of "for"
+namestmp = regexp(actall.acts,'(?<=for )[\w ]+\w','match','once');
+projnames = namestmp(cstrfind(namestmp,'\w+'));
+% extract additional project names after occurrence of coding
+namestmp = regexp(actall.acts,'(?<=coding )(?!for )[\w ]+\w','match','once');
+projnames = unique([projnames(:);namestmp(cstrfind(namestmp,'\w+'))]);
+namestmp = regexp(actall.acts,'(?<=programming )(?!for )[\w ]+\w','match','once');
+projnames = unique([projnames(:);namestmp(cstrfind(namestmp,'\w+'))]);
+% extract additional project names after occurrence of thinking about
+namestmp = regexp(actall.acts,'(?<=thinking about )[\w ]+\w','match','once');
+projnames = unique([projnames(:);namestmp(cstrfind(namestmp,'\w+'))]);
+
+% assumption is that the above was sufficient to find all project names
+
+% exclude meeting names by hand, because they are not projects
+% leave names of individuals in, because they stand for something that I
+% have done for the individuals probably for a particular project
+projnames = setdiff(projnames,[acls.grmeet(:);acls.instmeet(:)]);
+nproj = length(projnames);
+
+% equivalence between project names: 
+% [speeddem, dem, free energy]
+% [general, background]
+% [rrnns, fernns]
+% [placecells, eduardo]
+prjeq = {'dem','free energy','background',...
+         'fernns','eduardo'};
+projNames = setdiff(projnames,prjeq);
+prji = nan(nproj,1);
+for p = 1:nproj
+    switch projnames{p}
+        case {'dem','free energy'}
+            prji(p) = cstrfind(projNames,'speeddem');
+        case 'background'
+            prji(p) = cstrfind(projNames,'general');
+        case 'fernns'
+            prji(p) = cstrfind(projNames,'rrnns');
+        case 'eduardo'
+            prji(p) = cstrfind(projNames,'placecells');
+        otherwise
+            prji(p) = cstrfind(projNames,projnames{p});
+    end
+end
+nprojeq = length(projNames);
+
+% find all weeks:
+% find first Monday
+for d = 1:10
+    if weekday(actdays(d))==2
+        break
+    end
+end
+d1 = d;
+
+% find last Friday
+for d = 0:10
+    if weekday(actdays(end-d))==6
+        break
+    end
+end
+dend = ndays-d;
+
+% how many weeks are there?
+nweeks = ceil((actdays(dend)-actdays(d1))/7);
+
+% find all activities associated with found projects
+projfracs = zeros(nweeks,nprojeq+1); % one additional entry for groupmeetings
+for w = 1:nweeks
+    dweek = actdays(d1) + (w-1)*7;
+    dweek = find(actdays>=dweek & actdays<dweek+7);
+    if ~isempty(dweek)
+        % concatenate all activities of that week
+        actweek = cell(200,1);
+        dursweek = nan(200,1);
+        cnt = 0;
+        for d = dweek
+            na = size(actdaily(d).durs,1);
+            actweek(cnt+(1:na)) = lower(actdaily(d).acts);
+            dursweek(cnt+(1:na)) = actdaily(d).durs;
+            cnt = cnt + na;
+        end
+        actweek = actweek(1:cnt);
+        dursweek = dursweek(1:cnt);
+        
+        weekdurs = zeros(1,nprojeq+1);
+        weekdurs(end) = sum(dursweek(cstrfind(actweek,acls.grmeet)));
+        
+        % get project durations for that week
+        % note that this is the simplest way of selecting project
+        % durations simply by the occurrence of the project name, but it is
+        % not always the case that the project name leads to a project,
+        % e.g.: "meeting with Burak" could have been about anything, it
+        % appears to be a reasonably good heuristic though, because these
+        % things usually have a short duration compared to actual projects
+        for p = 1:nproj
+            weekdurs(prji(p)) = weekdurs(prji(p)) + ...
+                sum(dursweek(cstrfind(actweek,projnames{p})));
+        end
+        projfracs(w,:) = weekdurs/sum(weekdurs);
+    end
+end
+
+% a figure about the weekly fractions of time I spent in different projects
+vis = initvis([],[140  680 1700 420]);
+vis.bars = bar(projfracs,1,'stacked');
+ylim([0,1])
+xlim([0.5,nweeks+.5])
+
+legend([projNames,{'groupmeetings'}],'Location','EastOutside')
+xlabel('weeks')
+ylabel('fraction of project time')
+
+
+%% show activities of a particular week
+w = 35;
+dweek = actdays(d1) + (w-1)*7;
+dweek = find(actdays>=dweek & actdays<dweek+7);
+
+% concatenate all activities of that week
+actweek = cell(200,1);
+dursweek = nan(200,1);
+cnt = 0;
+for d = dweek
+    na = size(actdaily(d).durs,1);
+    actweek(cnt+(1:na)) = lower(actdaily(d).acts);
+    dursweek(cnt+(1:na)) = actdaily(d).durs;
+    cnt = cnt + na;
+end
+actweek = actweek(1:cnt);
+dursweek = dursweek(1:cnt);
