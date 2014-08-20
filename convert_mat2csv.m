dcebf7c8e7c914433f4ca002c174b47defdbdb51
Sebastian Bitzer <official@sbitzer.eu>
2014-08-20 19:02:55 +0200

    created first version of activitylog.csv
    I had committed that in Linux before, but somehow git did not remember.
    I also moved the git repository to a new folder. Is this why all files
    are listed as changed again, even though they did not change?

--- convert_mat2csv.m
+++ convert_mat2csv.m
@@ -2,9 +2,9 @@
 files = dir('activitylog_*.mat');
 ndays = length(files);
 
-nact = 0;
-actdaily = struct('times',[],'durs',[],'acts',[],'day',[]);
-for d = 741:743
+outfile = fopen('activitylog.csv', 'w');
+
+for d = 1:ndays
     fdata = load(files(d).name);
 
     day  = floor(fdata.jtimes(1));
@@ -18,21 +18,25 @@ for d = 741:743
         negind = find(durs < 0);
         for i = 1:length(negind)
             disp('Negative duration found! context:')
-            fprintf(1, 'activity string: %s\n', acts{negind})
-            fprintf(1, 'start t: %s\n', datestr(startt(negind(i))))
-            fprintf(1, 'end   t: %s\n', datestr(endt(negind(i))))
-            fprintf(1, 'end+1 t: %s\n', datestr(endt(negind(i)+1)))
+            fprintf(1, '%s, %s, %s\n', datestr(startt(negind(i)-1)), datestr(endt(negind(i)-1)), acts{negind(i)-1})
+            fprintf(1, '%s, %s, %s\n', datestr(startt(negind(i))), datestr(endt(negind(i))), acts{negind(i)})
+            fprintf(1, '%s, %s, %s\n', datestr(startt(negind(i)+1)), datestr(endt(negind(i)+1)), acts{negind(i)+1})
             newtstr = input('new time: ', 's');
-            usertime = regexp(newtstr,'(?<=@)[0-2]\d:[0-5]\d','match');
-            usertime = [str2double(usertime{1}(1:2)),str2double(usertime{1}(4:5))];
+            usertime = [str2double(newtstr(1:2)),str2double(newtstr(4:5))];
             endt(negind(i)) = day + (usertime(1)*60+usertime(2))/24/60;
+            if nact > negind(i)
+                startt( negind(i)+1 ) = endt(negind(i));
+            end
+            fprintf(1, 'new end t: %s\n', datestr(endt(negind(i))))
         end
+        durs = endt - startt;
+        assert( ~any( durs < 0) )
     end
 
     for a = 1:nact
-        fprintf(1, '%s, %s, %s\n', datestr(startt(a)), datestr(endt(a)), acts{a})
+        fprintf(outfile, '%.5f, %.5f, %s\n', startt(a), endt(a), acts{a})
     end
-    fprintf(1, '\n')
 
 end
 
+fclose(outfile)
