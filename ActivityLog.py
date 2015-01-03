ed5baf147338162977a615e8e200213c5485f490
Sebastian Bitzer <official@sbitzer.eu>
2015-01-03 18:33:56 +0100

    user interface works
    - found cmd-module to be perfect as user interface
    - now use duration instead of end-time in DB, because duration is what
      I want for most later analyses
    - completed basic functionality including check and fix for negative
      durations
    - now add some basic entries in jobs title when initialising DB
    - use short prompts first, but allow 'help'
    - fixed a couple of bugs:
      - the §-symbol made problems so I replaced it with &
      - figured out that you cannot use placeholders (?) for table and column
        names in sqlite calls
      - forgot to switch PARSE_DECLTYPES on when making new database

--- ActivityLog.py
+++ ActivityLog.py
@@ -9,6 +9,8 @@ import sqlite3
 import re
 import itertools
 import datetime as dt
+import cmd
+import random
 
 
 def isSQLite3(filename):
@@ -54,19 +56,31 @@ def writeTestEntries(alog):
     alog.dbcur.execute(
         "INSERT INTO dictionary (word, tab_name, tab_id) VALUES('BAttM', 'projects', 2)")
 
+    now = dt.datetime.now()
+    for i in range(7):
+        dur = random.random() * 1000
+        datetime = now + dt.timedelta(minutes = 2*i - 2*7)
+        alog.dbcur.execute(
+            "INSERT INTO jobs (start, duration, activity) VALUES (?, ?, 1)",
+            (datetime, dur))
+
     alog.dbcon.commit()
 
 
-class ActivityLog(object):
+class ActivityLog(cmd.Cmd):
     """A class which defines and manipulates an activity log"""
 
     # this separates the three parts of a job string:
     # activity[ with people][ for project|org|person]
-    act_re = re.compile('([\w ]+?)(?: with ([\w ,]+?))?(?:$| for ([\w ,]+))')
+    act_re = re.compile('([\w ]+?)(?: with ([\w ,]+?))?(?:$| (?:for|about) ([\w ,]+))')
+    time_re = re.compile('([\w ,]+)?(?:@(\d+):(\d+))?')
 
-    lastjob = ('', dt.datetime.min)
+    # (id of last row in jobs, start timestamp in that row)
+    lastjob = (None, None)
 
     def __init__(self, dbname):
+        cmd.Cmd.__init__(self)
+
         dbstate = isSQLite3(dbname)
         if dbstate == 1:
             self.dbcon = sqlite3.connect(dbname, detect_types=sqlite3.PARSE_DECLTYPES)
@@ -77,7 +91,7 @@ class ActivityLog(object):
         else:
             ans = raw_input('No file with that name exists. Make new database (y/n)? ')
             if ans.lower() == 'y':
-                self.dbcon = sqlite3.connect(dbname)
+                self.dbcon = sqlite3.connect(dbname, detect_types=sqlite3.PARSE_DECLTYPES)
                 self.dbcur = self.dbcon.cursor()
                 self.dbcur.execute("PRAGMA foreign_keys = ON")
 
@@ -138,7 +152,7 @@ class ActivityLog(object):
             "CREATE TABLE jobs ("
                 "id INTEGER PRIMARY KEY,"
                 "start TIMESTAMP NOT NULL UNIQUE,"
-                "end TIMESTAMP NOT NULL UNIQUE,"
+                "duration REAL,"
                 "activity INTEGER NOT NULL,"
                 "FOREIGN KEY(activity) REFERENCES activities(id) )" )
 
@@ -178,54 +192,60 @@ class ActivityLog(object):
         corrinput = False
 
         while not corrinput:
-            response = raw_input("You can now provide a shorter version of %s as "
-                "label, \nif you wish. If you currently add a person or project\n"
-                "to the database, you may assign that to an organisation.\n"
-                "Provide this as 'label § organisation'! You can leave\n"
-                "anyone of them empty as long as you provide the '§'.\n"
-                "Simply press ENTER to use %s as label and no organisation:\n"
-                % (name, name))
+            response = raw_input("label/organisation (or type help): ")
+            if response.strip().lower() == "help":
+                response = raw_input("You can now provide a shorter version of %s as label, \n"
+                    "if you wish. If you currently add a person or project\n"
+                    "to the database, you may assign that to an organisation.\n"
+                    "Provide this as 'label & organisation'! You can leave\n"
+                    "anyone of them empty as long as you provide the '&'.\n"
+                    "Simply press ENTER to use %s as label and no organisation:\n"
+                    % (name, name))
+
             if response == '':
                 corrinput = True
                 label = name
                 org = None
             else:
-                match = re.match('(\w*)\s*§\s*(\w*)', response)
+                match = re.match('\s*(\w*)\s*(?:&\s*(\w*)\s*)?', response)
                 if match == None:
                     print("DID NOT RECOGNISE FORMAT!\n"
                           "Make sure you follow the instructions below!\n"
-                          "Only use alpha-numeric names on either side of the §!\n\n")
+                          "Only use alpha-numeric names without spaces on either side of the &!\n\n")
                     continue
                 else:
                     corrinput = True
 
-                    if match.group(1) == '':
+                    if match.group(1) == None:
                         label = name
                     else:
                         label = match.group(1)
 
-                    if match.group(2) == '':
+                    if match.group(2) == None:
                         org = None
                     else:
                         org = self.getIDfromDict(match.group(2), 'organisations')
 
-        try:
-            if (table == 'people' or table == 'projects') and not org == None:
-                self.dbcur.execute(
-                    "INSERT INTO ? (name, label) "
-                    "VALUES(?, ?)", (table, name, label))
-            else:
-                self.dbcur.execute(
-                    "INSERT INTO ? (name, label, org) "
-                    "VALUES(?, ?, ?)", (table, name, label, org))
+        # may FAIL because label not unique!
+        if (table == 'people' or table == 'projects') and not org == None:
+            self.dbcur.execute(
+                "INSERT INTO '%s' (name, label, org) "
+                "VALUES(?, ?, ?)" % table, (name, label, org))
+        else:
+            self.dbcur.execute(
+                "INSERT INTO '%s' (name, label) "
+                "VALUES(?, ?)" % table, (name, label))
 
-            self.dbcon.commit()
+        self.dbcon.commit()
 
-            newid = alog.dbcur.lastrowid
-        # TODO: specify proper exception to catch
-        except:
-            print("Database insertion failed: skipping\n")
-            newid = None
+        newid = alog.dbcur.lastrowid
+
+        # add also to dictionary
+        self.dbcur.execute(
+            "INSERT INTO dictionary (word, tab_name, tab_id) "
+            "VALUES(?, ?, ?)", (name, table, newid))
+
+        self.dbcon.commit()
 
         return newid
 
@@ -236,35 +256,37 @@ class ActivityLog(object):
             for i, tab in enumerate(table[1:]):
                 tabstr = tabstr + ', %d: %s' % (i+1, tab)
 
-            response = raw_input("I'm sorry. Does %s belong into %s?:\n" % (name, tabstr))
+            response = raw_input("Does %s belong into %s?:\n" % (name, tabstr))
             table = table[int(response)]
         else:
             table = table[0]
 
-        # misspelled, new alias, or new entry?
-        response = raw_input("There is no %s in %s. Add in %s (1), "
-            "add as alias (2: name) or check DB (3)?\n"
-            "If you made a typo, you can also just provide the name again "
-            "without the typo:\n" % (name, table, table))
+        response = raw_input("%s unknown. Resolve (or type help): " % name)
+        if response.strip().lower() == "help":
+            # misspelled, new alias, or new entry?
+            response = raw_input("There is no %s in %s. Add in %s (1),\n"
+                "add as alias (2: realname) or check DB (3)? If you\n"
+                "made a typo, you can also just provide the name again\n"
+                "without the typo:\n" % (name, table, table))
 
         if response[0] == '1':
             return (self.addBaseType(name, table), table)
         elif response[0] == '2':
-            nameintab = response[2:]
-            tab_id = self.getIDfromDict(nameintab, table)
+            nameintab = response[2:].strip()
+            idtab = self.getIDfromDict(nameintab, table)
             self.dbcur.execute(
                 "INSERT INTO dictionary (word, tab_name, tab_id) "
-                "VALUES(?, ?, ?)", (name, table, tab_id))
+                "VALUES(?, ?, ?)", (name, table, idtab[0]))
             self.dbcon.commit()
 
-            return (tab_id, table)
+            return idtab
         elif response[0] == '3':
             pass
             # print all table entries repeating those in the end that start
             # with the same letters and redo
 
         else:
-            return self.getIDfromDict(response, table)
+            return self.getIDfromDict(response.strip(), table)
 
 
     def getIDfromDict(self, name, table):
@@ -280,7 +302,7 @@ class ActivityLog(object):
         self.dbcur.execute(
             "SELECT tab_id, tab_name "
             "FROM dictionary "
-            "WHERE word = ? and tab_name IN %s" % placeholders, (name,)+ table )
+            "WHERE word = ? and tab_name IN (%s)" % placeholders, (name,)+ table )
         result = self.dbcur.fetchone()
         if result == None:
             # name is not in dictionary:
@@ -306,17 +328,21 @@ class ActivityLog(object):
 
     def parseForinfo(self, forstr):
 
-        # project-organisation list
-        polist = map( lambda s: s.split(', '), forstr.split(' and ') )
-        polist = list( itertools.chain.from_iterable(polist) )
+        # project-person-organisation list
+        ppolist = map( lambda s: s.split(', '), forstr.split(' and ') )
+        ppolist = list( itertools.chain.from_iterable(ppolist) )
 
+        pids = []
         pjids = []
         orgids = []
-        for po in polist:
-            forid, fortab = self.getIDfromDict(po, ('projects', 'organisations'))
+        for ppo in ppolist:
+            forid, fortab = self.getIDfromDict(ppo, ('projects', 'people',
+                                                     'organisations'))
 
             if fortab == 'projects':
                 pjids.append(forid)
+            elif fortab == 'people':
+                pids.append(forid)
             else:
                 orgids.append(forid)
 
@@ -325,18 +351,17 @@ class ActivityLog(object):
         if len(orgids) == 0:
             orgids = None
 
-        return pjids, orgids
+        return pjids, pids, orgids
 
 
     def parseJobStr(self, jobstr):
         """parses the raw input from the user"""
 
-        # act
-        # act with {people}
-        # act with {people} for project
-        # act for project
-        # act for org
-        # act for person
+        # act(e.g. break)
+        # act(e.g. meeting) with {people}
+        # act(e.g. meeting) with {people} for {project|org|person}
+        # act(e.g. reading) for {project|org|person}
+        # act(e.g. thinking) about {people|projects}
 
         # extract activity and modifier
         match = self.act_re.match(jobstr)
@@ -352,18 +377,26 @@ class ActivityLog(object):
 
             if match.group(2) != None:
                 people = self.parsePeople(match.group(2))
+            else:
+                people = []
 
             if match.group(3) != None:
-                projects, orgs = self.parseForinfo(match.group(3))
+                projects, person, orgs = self.parseForinfo(match.group(3))
+            else:
+                person = []
+
+            people = people + person
+            if len(people) == 0:
+                people = None
 
         return act, people, projects, orgs
 
 
-    def addJobToDB(self, start, end, act, projects=None, people=None, orgs=None):
+    def addJobToDB(self, startdt, act, projects=None, people=None, orgs=None):
         # add job to jobs table and retrieve its id
         self.dbcur.execute(
-            "INSERT INTO jobs (start, end, activity) "
-            "VALUES(?, ?, ?)", (start, end, act) )
+            "INSERT INTO jobs (start, activity) "
+            "VALUES(?, ?)", (startdt, act) )
         jobid = self.dbcur.lastrowid
 
         # add associated project(s), if exist
@@ -389,15 +422,164 @@ class ActivityLog(object):
 
         self.dbcon.commit()
 
+        return jobid
 
-    def processJob(self, date, jobstr):
+
+    def addDurationToJob(self, enddt):
+
+        if self.lastjob[0] != None:
+            duration = (enddt - self.lastjob[1]).total_seconds()
+
+            # everything is fine
+            if duration >= 0:
+                self.dbcur.execute(
+                    "UPDATE jobs SET duration=? WHERE id=?", (duration,
+                                                              self.lastjob[0]))
+
+                self.dbcon.commit()
+
+            # resolve impossible negative duration by asking user
+            else:
+                validtime = False
+
+                # get start time of job previous to lastjob
+                self.dbcur.execute("SELECT id, start FROM jobs WHERE id < ? "
+                    "ORDER BY start DESC", (self.lastjob[0],))
+                oldjob = self.dbcur.fetchone()
+
+                while not validtime:
+                    response = raw_input("Negative duration! Fix (or type help): ")
+
+                    # help needed
+                    if response.strip().lower() == "help":
+                        response = raw_input("From the given times the last job has a negative\n"
+                        "duration. This cannot be. Please provide a new time.\n"
+                        "To provide a new time for the new input just give the\n"
+                        "time with an @. To provide a new time for the last input\n"
+                        "prepend the time with the word 'last'! Note that, if you\n"
+                        "choose to change the last input, the new time must be \n"
+                        "between %02d:%02d and %02d:%02d. Syntax examples:\n"
+                        "last @12:34\n"
+                        "@14:49\n" % (oldjob[1].hour, oldjob[1].minute,
+                                      enddt.hour, enddt.minute))
+
+                    # extract time
+                    match = self.time_re.match(response)
+
+                    # invalid format
+                    if ( match == None or match.group(2) == None or
+                        match.group(3) == None ):
+                        print "Given input has invalid format!\n"
+                    else:
+                        hours = int(match.group(2))
+                        mins = int(match.group(3))
+                        if hours < 0 or hours >= 24 or mins < 0 or mins >= 60:
+                            print "Given time is invalid!\n"
+                            continue
+
+                        # time only: change enddt
+                        if match.group(1) == None:
+                            enddt = dt.datetime.combine(enddt.date(),
+                                                        dt.time(hours, mins))
+                            if enddt > self.lastjob[1]:
+                                validtime = True
+                                self.addDurationToJob(enddt)
+                            else:
+                                print "New time is still before start of last job!\n"
+
+                        # last: change time of lastjob
+                        elif match.group(1).strip() == 'last':
+                            # is new time between oldstart and enddt?
+                            newdt = dt.datetime.combine(self.lastjob[1].date(),
+                                                        dt.time(hours, mins))
+                            if oldjob[1] < newdt and newdt < enddt:
+                                validtime = True
+
+                                # update lastjob start in DB and self.lastjob
+                                self.lastjob = (self.lastjob[0], newdt)
+                                self.dbcur.execute("UPDATE jobs SET start=? WHERE id=?",
+                                                   (newdt, self.lastjob[0]))
+
+                                # update duration of job with oldid in DB
+                                duration = (newdt - oldjob[1]).total_seconds()
+                                self.dbcur.execute("UPDATE jobs SET duration=? "
+                                    "WHERE id=?", (duration, oldjob[0]))
+
+                                self.dbcon.commit()
+
+                                self.addDurationToJob(enddt)
+                            else:
+                                print "New time is not between %02d:%02d and %02d:%02d!\n" % (
+                                    oldjob[1].hour, oldjob[1].minute,
+                                    enddt.hour, enddt.minute)
+
+        return enddt
+
+
+    def processJob(self, datetime, jobstr):
         act, people, projects, orgs = self.parseJobStr(jobstr)
 
+        jobid = self.addJobToDB(datetime, act, projects, people, orgs)
 
+        return jobid
+
+
+    def getTime(self, instr):
+        indt = dt.datetime.today()
+
+        if len(instr) == 0:
+            jobstr = None
+        else:
+            match = self.time_re.match(instr)
+            jobstr = match.group(1).strip()
+
+            if match.group(2) != None:
+                # check whether time is valid
+                hours = int(match.group(2))
+                mins = int(match.group(3))
+
+                if hours >= 0 and hours < 24 and mins >= 0 and mins < 60:
+                    indt = dt.datetime.combine(dt.date.today(),
+                                               dt.time(hours, mins))
+                else:
+                    print "invalid time: taking current time instead\n"
+
+        indt = self.addDurationToJob(indt)
+
+        return indt, jobstr
+
+
+    # pre-loop for greetings and check for last job without duration
+    def preloop(self):
+        pass
+
+
+    # handling of standard activity input
+    def default(self, instr):
+        indt, jobstr = self.getTime(instr)
+
+        # process job
+        lastid = self.processJob(indt, jobstr)
+        self.lastjob = (lastid, indt)
+
+        print indt.strftime('%H:%M')
+
+
+    # delete or overwrite previous jobs
+    def do_del(self, instr):
+        pass
+
+
+    # close session
+    def do_feierabend(self, instr):
+        indt, jobstr = self.getTime(instr)
+
+        return True
 
 
 if __name__ == "__main__":
     alog = ActivityLog('test.sqlite')
+    alog.cmdloop()
 
 #    if alog.dbcon != None:
 #        alog.dbcon.close()
