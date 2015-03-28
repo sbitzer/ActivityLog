e9c0387a5c3eb57230df8eae0fd26a27e6eb47bd
Sebastian Bitzer <official@sbitzer.eu>
2015-03-28 16:36:05 +0100

    First version of the ActivityLog filler function
    This takes a semicolon-separated file containing a list of log entries converted
    from Matlab and puts the entries in an ActivityLog. The standard functions of
    the ActivityLog are used and the user has to clarify input that is unclear, as
    when running the ActivityLog directly.
    
    The filler breaks on errors and resumes work when called again. This is
    implemented by storing the last successful state (the last row) in
    lastrowinfo.pkl. The user can also break the process by pressing Ctrl-c followed
    by Enter in one of the user input dialogues. Filling will then also be resumed
    the next time the filler is called.
    
    Seems to work.

--- fillActivityLog.py
+++ fillActivityLog.py
@@ -5,10 +5,117 @@ Created on Tue Jan 27 21:41:18 2015
 @author: Zibbi
 """
 from ActivityLog import ActivityLog
+import csv
+import datetime
+import pickle
+import sys
+import traceback
+
+def convMatlabdate2Datetime(matdate):
+    # Matlab's and Python's definition of the ordinal date format differ
+    # by 1 year and 1 day
+    dt = datetime.datetime.fromordinal(int(matdate) - 366)
+    H = matdate % 1 * 24
+    M = H % 1 * 60
+    S = M % 1 * 60
+
+    dt = dt.replace(hour=int(H), minute=int(M), second=int(S))
+
+    return dt
+
 
 if __name__ == "__main__":
     alog = ActivityLog('test.sqlite')
 
+    # endt may change when faulty input is corrected during computation of durations
+    endt_ischanged = False
+    endt_new = datetime.datetime.now()
+
+    try:
+        fi = open('lastrowinfo.pkl','r')
+    except IOError:
+        initrow = 1
+    else:
+        lastrowinfo = pickle.load(fi)
+        initrow = lastrowinfo[0]
+        fi.close()
+
+    if initrow > 1:
+        print "Resuming with row %d!" % initrow
+        print "---------------------"
+
     # loop over entries in the csv file
-    # extract datetime for job
-    # use a function which takes jstr and datetime to add job
+    with open('activitylog_test.csv', 'rb') as alogfile:
+        reader = csv.reader(alogfile, delimiter=';', skipinitialspace=True)
+        count = 0
+        for row in reader:
+            count = count + 1
+
+            if count < initrow:
+                continue
+
+            # extract datetimes for job
+            if endt_ischanged:
+                startt = endt_new
+            else:
+                startt = convMatlabdate2Datetime(float(row[0]))
+            endt = convMatlabdate2Datetime(float(row[1]))
+
+            # if you resume a previous run
+            if count == initrow & count > 1:
+                # check that start time of this job and end time of stored last
+                # job match
+                if startt == lastrowinfo[3]:
+                    # set lastjob to that stored in lastrowinfo
+                    alog.lastjob = (lastrowinfo[1], lastrowinfo[2])
+                else:
+                    print "Start time of the job to resume and end time"
+                    print "of stored previous job do not match."
+                    print "Something wrong. Shutting down."
+                    break
+
+            print startt, row[2]
+
+            # separate out jobstr from time string
+            jobstr = alog.time_re.match(row[2]).group(1).strip()
+
+            try:
+                # add job to DB
+                jobid = alog.processJob(startt, jobstr)
+            except:
+                print "Error in row %d when adding job." % count
+                print "row info:"
+                print row
+                print "error info:"
+                traceback.print_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
+                print "Shutting down."
+
+                fi = open('lastrowinfo.pkl','w')
+                pickle.dump([count, alog.lastjob[0], alog.lastjob[1], startt], fi)
+                fi.close()
+
+                break
+            else:
+                # update lastjob
+                alog.lastjob = (jobid, startt)
+
+                try:
+                    # add duration
+                    endt_new = alog.addDurationToJob(endt)
+                except:
+                    print "Error in row %d when adding duration." % count
+                    print "row info:"
+                    print row
+                    print "error info:"
+                    traceback.print_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
+                    print "Add duration manually, update endt in lastrowinfo, " \
+                        "if necessary, then resume!"
+                    print "Shutting down."
+
+                    fi = open('lastrowinfo.pkl','w')
+                    pickle.dump([count+1, jobid, startt, endt], fi)
+                    fi.close()
+
+                    break
+                else:
+                    endt_ischanged = endt_new != endt
