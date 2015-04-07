46d11a4031fb51b12eb6724a5b23a643b007de0a
Sebastian Bitzer <official@sbitzer.eu>
2015-04-07 10:12:07 +0200

    added absolute paths so that I can run from Desktop

--- run_ActivityLog.py
+++ run_ActivityLog.py
@@ -6,10 +6,16 @@ Created on Mon Apr  6 21:01:20 2015
 @author: Sebastian Bitzer (official@sbitzer.eu)
 """
 
+import sys
+
+sys.path.append('/home/bitzer/Projekte/activitylog')
+
 from ActivityLog import ActivityLog
 
-alog = ActivityLog('sebsalog.sqlite')
+alog = ActivityLog('/home/bitzer/Projekte/activitylog/sebsalog.sqlite')
 alog.cmdloop()
 
 if alog.dbcon != None:
     alog.dbcon.close()
+
+raw_input('Press enter to close ...')
